dc9deb0e1143ea13ef48fe7154821a80755d8fc2
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-06-11 11:14:48 +0000

    Render Multiple Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from 'react';
 
-const App = () => {
+const Project = ({ name, description }) => {
   return (
-    <div id="main"></div>
-  )
-}
+    <div className="project">
+      <h1 data-ns-test="project-name">{name}</h1>
+      <h6 data-ns-test="project-description">{description}</h6>
+    </div>
+  );
+};
 
+function App() {
+  const projects = [
+    { name: 'Project 1', description: 'This is my first dream project.' },
+    { name: 'Project 2', description: 'This is my second dream project.' },
+    // Add more projects here
+  ];
+
+  return (
+    <div className="ns-wrapper" id='main'>
+      {projects.map((project) => (
+        <Project key={project.name} {...project} />
+      ))}
+    </div>
+  );
+}
 
 export default App;
